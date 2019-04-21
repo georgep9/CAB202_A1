b0b0f655a19a5cd657c23ac332046809b8ef57f4
George Patterson <g-patt@outlook.com>
2019-04-21 23:30:05 +1000

    fixed collision with charging station

--- vacuum.c
+++ vacuum.c
@@ -12,7 +12,7 @@
 #define PI 3.14159265
 
 #define SIZE 13
-static double vac_x, vac_y;
+static double x_centre, y_centre;
 static int direction;
 static bool in_motion;
 static double return_to_base;
@@ -39,8 +39,8 @@ const char* vacuum_bitmap =
 void setup_vacuum(){
     int w, h;
     get_screen_size(&w, &h);
-    vac_x = w/2;
-    vac_y = h/2;
+    x_centre = w/2;
+    y_centre = h/2;
     direction = 90;
     in_motion = false;
     return_to_base = false;
@@ -50,8 +50,8 @@ void setup_vacuum(){
 
 void draw_vacuum(){
     // get top left pixel positions
-    int left = vac_x - SIZE/2;
-    int top = vac_y - SIZE/2;
+    int left = x_centre - SIZE/2;
+    int top = y_centre - SIZE/2;
     // loops through vacuum bitmap 
     for (int j = 0; j < SIZE; j++){
         for (int i = 0; i < SIZE; i++){
@@ -78,18 +78,27 @@ void rotate_vacuum(){
     }
 }
 
-bool collides_with(char object[]){
+// check if the vacuum at a given position, collides with described object
+bool collides_with(int x, int y, char object[]){
 
+    x -= SIZE/2; // left position of vacuum at given x
+    y -= SIZE/2; // top position of vacuum at given y
+
+    // declare variables for described oject
     int x1, y1, w1, h1;
     char* object_bitmap;
 
+    // if object is "station"
     if (strcmp(object,"station")==0){
-        get_station_size(&w1, &h1);
-        get_station_coords(&x1 - w1, &y1 - h1);
-        object_bitmap = get_station_bitmap();
+        get_station_size(&w1, &h1); // get width & height
+        get_station_coords(&x1, &y1); // get its coords
+        x1 -= w1/2; // left position of object 
+        y1 -= h1/2; // right positon of object
+        object_bitmap = get_station_bitmap(); // get object bitmap
     }
 
-    return pixel_collision(vac_x, vac_y, SIZE, SIZE, (char*)vacuum_bitmap, 
+    // return bool result of whether pixel collision occurs (collision.c)
+    return pixel_collision(x, y, SIZE, SIZE, (char*)vacuum_bitmap, 
                             x1, y1, w1, h1, object_bitmap);
 }
 
@@ -100,19 +109,19 @@ void move_vacuum(){
 
     if (in_motion){
         double rad = direction * PI/180;
-        // get new x and y
-        double new_x = vac_x + 0.2*cos(rad);
-        double new_y = vac_y + 0.2*sin(rad);
+        // calculate new x and y
+        double new_x = x_centre + 0.2*cos(rad);
+        double new_y = y_centre + 0.2*sin(rad);
 
         // check if collides with station
-        if (collides_with("station") == false){
+        if (collides_with(new_x,new_y,"station") == false){
             // move x if vacuum doesn't collide with vertical borders
-            if (within_borders(new_x, vac_y, SIZE, SIZE)){
-                vac_x = new_x;
+            if (within_borders(new_x, y_centre, SIZE, SIZE)){
+                x_centre = new_x;
             } else { rotate_vacuum(); } // otherwise rotate
             // move y if vacuum doesn't collide with horizontal borders
-            if (within_borders(vac_x, new_y, SIZE, SIZE)){
-                vac_y = new_y;
+            if (within_borders(x_centre, new_y, SIZE, SIZE)){
+                y_centre = new_y;
             } else { rotate_vacuum(); } // otherwise rotate
         } else { rotate_vacuum(); } // otherwise rotate
     }
@@ -137,21 +146,32 @@ void vacuum_collides(){
 
 void control_vacuum(char key){
     switch(key){
+        
         case 'i':
-            if (within_borders(vac_x, vac_y-1, SIZE, SIZE)) { vac_y--; }
+            if (within_borders(x_centre, y_centre-1, SIZE, SIZE)) { 
+                y_centre--; 
+            }
             break;
         case 'j':
-            if (within_borders(vac_x-1, vac_y, SIZE, SIZE)) { vac_x--; }
+            if (within_borders(x_centre-1, y_centre, SIZE, SIZE)) { 
+                x_centre--; 
+            }
             break;
         case 'k':
-            if (within_borders(vac_x, vac_y+1, SIZE, SIZE)) { vac_y++; }
+            if (within_borders(x_centre, y_centre+1, SIZE, SIZE)) { 
+                y_centre++; 
+            }
             break;
         case 'l':
-            if (within_borders(vac_x+1, vac_y, SIZE, SIZE)) { vac_x++; }
+            if (within_borders(x_centre+1, y_centre, SIZE, SIZE)) { 
+                x_centre++; 
+            }
             break;
+
         case 'p':
             if (in_motion){ in_motion = false; }
-            else{ in_motion = true; }
+            else { in_motion = true; }
+
         case 'b':
             return_to_base = true;
     }
@@ -183,7 +203,7 @@ void set_vacuum_x(){
     int left_bound = 1;
     int right_bound = screen_width() - 1;
     if (x-SIZE/2 > left_bound && x+SIZE/2 < right_bound){
-        vac_x = x;
+        x_centre = x;
     }
 }
 
@@ -192,13 +212,13 @@ void set_vacuum_y(){
     int top_bound = 5;
     int bottom_bound =  screen_height() - 3;
     if (y-SIZE/2 > top_bound && y+SIZE/2 < bottom_bound){
-        vac_y = y;
+        y_centre = y;
     }
 }
 
 void get_vacuum_coords(double* x, double*y){
-    *x = vac_x;
-    *y = vac_y;
+    *x = x_centre;
+    *y = y_centre;
 }
 
 char* get_vacuum_bitmap(){
